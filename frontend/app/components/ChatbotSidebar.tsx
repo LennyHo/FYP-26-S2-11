a9520eae42f5460590a2af1b47e92d455d79be2a
Yean Kit He Yingjie Ho <60606097+LennyHo@users.noreply.github.com>
2026-05-15 14:51:10 +0800

    fix: correct Avy_logo path casing for Vercel build

--- frontend/app/components/ChatbotSidebar.tsx
+++ frontend/app/components/ChatbotSidebar.tsx
@@ -14,8 +14,8 @@ import {
 import SpeechControls from './SpeechControls';
 import QuickPrompts from './QuickPrompts';
 import DrinkRecCards from './DrinkRecCards';
-import avyLogo from '../../img/avy_logo/Group 2.svg';
-import avyIntroduction from '../../img/avy_logo/avy_introduction.svg';
+import avyLogo from '../../img/Avy_logo/Group 2.svg';
+import avyIntroduction from '../../img/Avy_logo/avy_introduction.svg';
 import menuData from '../../data/menu.json';
 
 // ===== TYPE DEFINITIONS =====
