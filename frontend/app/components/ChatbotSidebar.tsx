bf8c5f9d709d6148bc31bea39d24ba2121da2614
Yean Kit He Yingjie Ho <60606097+LennyHo@users.noreply.github.com>
2026-05-18 22:05:12 +0800

    -

--- frontend/app/components/ChatbotSidebar.tsx
+++ frontend/app/components/ChatbotSidebar.tsx
@@ -15,7 +15,7 @@ import SpeechControls from './SpeechControls';
 import QuickPrompts from './QuickPrompts';
 import DrinkRecCards from './DrinkRecCards';
 import avyLogo from '../../img/avy_logo/Group 2.svg';
-import avyIntroduction from '../../img/Avy_logo/avy_introduction.png';
+import avyIntroduction from '../../img/avy_introduction.png';
 import menuData from '../../data/menu.json';
 
 // ==== TYPE DEFINITIONS ====
