4fa8a5c117d850ade4fd7193ddb0a33999da4a3f
Yean Kit He Yingjie Ho <60606097+LennyHo@users.noreply.github.com>
2026-06-21 03:31:38 +0800

    improve the the ui button for avy section

--- view/app/components/pages/AvySection.tsx
+++ view/app/components/pages/AvySection.tsx
@@ -54,9 +54,6 @@ export default function AvySection() {
             onClick={() => window.dispatchEvent(new CustomEvent('openAvyChat'))}
           >
             Talk to Avy
-            <svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2.5" strokeLinecap="round" strokeLinejoin="round" aria-hidden="true">
-              <path d="M5 12h14M12 5l7 7-7 7" />
-            </svg>
           </button>
         </div>
 
